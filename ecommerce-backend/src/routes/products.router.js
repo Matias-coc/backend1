9d0036431353f894f03b6f9e0d8ffd69bc5650a2
Matias-coc <matiasing0422@gmail,com>
2026-08-02 17:33:03 -0300

    agregue las rutas de products

--- ecommerce-backend/src/routes/products.router.js
+++ ecommerce-backend/src/routes/products.router.js
@@ -2,5 +2,23 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send()
-})
+    res.send('listar productos')
+});
+
+router.get('/:pid', (req, res) => {
+    res.send(`Obtener productos ID: ${req.params.pid}`);
+});
+
+router.post('/', (req, res) => {
+    res.send('Crear producto')
+});
+
+router.put('/:pid', (req, res) => {
+    res.send(`Actualizar productos ID: ${req.params.pid}`);
+});
+
+router.delete('/:pid', (req, res) => {
+    res.send(`Eliminar productos ID: ${req.params.pid}`);
+});
+
+module.exports = router;
