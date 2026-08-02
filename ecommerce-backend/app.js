715aa5eeb6684cb259a3bdec321e1151b5b78d1c
Matias-coc <matiasing0422@gmail,com>
2026-08-02 16:54:30 -0300

    Primer commit

---
+++ ecommerce-backend/app.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const app = express();
+
+app.use(express.json()); 
+
+const productsRouter = require('./src/routes/products.router.js');
+const cartsRouter = require('./src/routes/carts.router.js')
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+app.listen(8080, () => {
+    console.log('Servidor corriendo en puerto 8080');
+});
---
+++ ecommerce-backend/src/routes/carts.router.js
@@ -0,0 +1,5 @@
+const express = require ('express');
+const app = express();
+
+app.post('/api/carts', (req, res) => {}); 
+app.get('/api/carts/:cid', (req, res) => {}); 
